0787614cc689f57b13dd589c1a261e39542e8364
clide7029 <70732345+clide7029@users.noreply.github.com>
2021-10-13 11:10:19 -0700

    added sound and img changes to music buttons

--- app.js
+++ app.js
@@ -33,9 +33,9 @@ function updateRuns(box, op) {
     box = team + '8';
     let cur = runs[box].innerHTML;
     let n;
-    if(op){
+    if (op) {
         n = parseInt(cur) + 1;
-    }else{
+    } else {
         n = parseInt(cur) - 1;
     }
     runs[box].innerHTML = n;
@@ -52,11 +52,40 @@ function plusRuns() {
 function minusRuns() {
     let box = getBox();
     let n = parseInt(runs[box].innerHTML) - 1;
-    runs[box].innerHTML = n;
-    updateRuns(box, 0);
+    if (n >= 0) {
+        runs[box].innerHTML = n;
+        updateRuns(box, 0);
+    }
+}
+
+function randomSong() {
+    let randS = Math.floor(Math.random() * 3);
+    let song;
+    if (randS == 0) {
+        song = new Audio("./sounds/buildup.mp3");
+    } else if (randS == 1) {
+        song = new Audio("./sounds/charge1.mp3");
+    } else if (randS == 2) {
+        song = new Audio("./sounds/charge2.mp3");
+    }
+    song.play();
+
+    let randI = Math.floor(Math.random() * 3);
+    let img = document.getElementById("logo");
+    if (randI == 0) {
+        img.src = "./images/megaphone.png";
+    } else if (randI == 1) {
+        img.src = "./images/sdchicken.jpg";
+    } else if (randI == 2) {
+        img.src = "./images/ups.png";
+    }
 }
 
 function chickenSong() {
     var song = new Audio("./sounds/chickendance.mp3");
     song.play();
+
+    let img = document.getElementById("logo");
+    img.src = "./images/sdchicken.jpg";
+
 }
